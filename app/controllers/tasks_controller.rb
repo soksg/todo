aaf8c9b2b5e280da0c70acb485a29c6f98696344
EC2 Default User <sk2361ksg@gmail.com>
2023-01-09 06:09:01 +0000

    [Add]タスク削除機能を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,6 +29,12 @@ class TasksController < ApplicationController
     end
   end
 
+  def destroy
+    @task=Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path
+  end
+
   private
   def task_params
     params.require(:task).permit(:title)
