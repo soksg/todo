bd6d51a6c2b0bfd18b07669fe578f56be8197deb
EC2 Default User <sk2361ksg@gmail.com>
2023-01-09 06:35:48 +0000

    [Add]コントローラの共通化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy]
+
   def index
     @tasks=Task.all
   end
@@ -17,11 +19,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task=Task.find(params[:id])
+    # @task=Task.find(params[:id])
   end
 
   def update
-    @task=Task.find(params[:id])
+    # @task=Task.find(params[:id])
     if @task.update(task_params)
       redirect_to root_path
     else
@@ -30,7 +32,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task=Task.find(params[:id])
+    # @task=Task.find(params[:id])
     @task.destroy
     redirect_to root_path
   end
@@ -38,5 +40,9 @@ class TasksController < ApplicationController
   private
   def task_params
     params.require(:task).permit(:title)
+  end
+
+  def set_task
+    @task=Task.find(params[:id])
   end
 end
